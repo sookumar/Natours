f94f6827a2e989d7cb7dc2b10aa19a4e274eeebf
sookumar <suraj623985@gmail.com>
2026-01-18 00:15:30 +0530

    created a tour schema

--- server.js
+++ server.js
@@ -16,6 +16,23 @@ mongoose
   })
   .then(() => console.log('DB connection successful'));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
 const app = require('./app');
 
 // console.log(process.env);
