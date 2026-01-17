98cc59515f51ad1589329b4d07dc0842cc6fefd2
sookumar <suraj623985@gmail.com>
2026-01-17 15:03:53 +0530

    Environments variable

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) middle wares
+  console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV=== 'development'){
+  app.use(morgan('dev')); 
+  
+}
 
-app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 
