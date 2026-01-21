73757c2f760a62e4272672c96458ce9a554b6190
sookumar <suraj623985@gmail.com>
2026-01-21 23:59:48 +0530

    filterd data

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -9,7 +9,20 @@ const Tour = require('../models/tourModel');
 ===================== */
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    console.log(req.query, queryObj);
+    const query = await Tour.find(req.query);
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
