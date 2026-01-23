20d4fab6ea026b38b0973217c9d06cb44dbdaadd
sookumar <suraj623985@gmail.com>
2026-01-23 15:56:38 +0530

    added fields limiting, which ensure the user only see the value which is in the field nothing else

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -38,6 +38,16 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      console.log(fields);
+
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     // Execute query
     const tours = await query;
 
