dabd6ea4665d29a853bb7704cd100294228d83a9
sookumar <suraj623985@gmail.com>
2026-01-26 13:58:10 +0530

    corrected the spelling mistake in bcrypt.hash

--- models/userModel.js
+++ models/userModel.js
@@ -40,7 +40,7 @@ userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
 
   // Hash the password with cost of 12
-  this.password = await bcrypt.has(this.password, 12);
+  this.password = await bcrypt.hash(this.password, 12);
 
   // Delete passwordConfirm field
   this.passwordConfirm = undefined;
