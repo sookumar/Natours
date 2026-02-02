7528bc24e70be0baa21c1040f4cb0be754993c12
sookumar <suraj623985@gmail.com>
2026-02-02 21:59:05 +0530

    Implementing Simple Nested Routes

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,6 +2,10 @@ const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  // Allow nested rotues
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+
   const review = await Review.create(req.body);
 
   res.status(201).json({
